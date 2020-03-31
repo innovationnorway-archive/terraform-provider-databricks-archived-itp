c070257094c794eae6de706081eaa61ae9c50efa
Joakim Bakke Hellum <joakim.bakke.hellum@innovasjonnorge.no>
2020-03-31 18:57:50 +0200

    Fix errors in `databricks_group_member` resource
    * Fix errors in `resourceDatabricksGroupMemberRead` function
    * Fix lint error AT001: missing CheckDestroy in databricks_group_member resource
    * Fix lint error AT003: acceptance test function name should include underscore

--- databricks/resource_databricks_group_member_test.go
+++ databricks/resource_databricks_group_member_test.go
@@ -6,18 +6,21 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/innovationnorway/go-databricks/groups"
 )
 
-func TestAccDatabricksGroupMember(t *testing.T) {
+func TestAccDatabricksGroupMember_basic(t *testing.T) {
 	resourceName := "databricks_group_member.test"
 	groupName := acctest.RandString(6)
 
 	resource.Test(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheck(t) },
-		Providers: testAccProviders,
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckDatabricksGroupMemberDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDatabricksGroupMember(groupName),
+				Config: testAccDatabricksGroupMemberBasic(groupName),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(resourceName, "parent_name", groupName),
 					resource.TestCheckResourceAttrSet(resourceName, "user_name"),
@@ -27,7 +30,38 @@ func TestAccDatabricksGroupMember(t *testing.T) {
 	})
 }
 
-func testAccDatabricksGroupMember(groupName string) string {
+func testAccCheckDatabricksGroupMemberDestroy(s *terraform.State) error {
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "databricks_group_member" {
+			continue
+		}
+
+		parentName := rs.Primary.Attributes["parent_name"]
+		groupName := rs.Primary.Attributes["group_name"]
+		username := rs.Primary.Attributes["user_name"]
+
+		principalName := groups.PrincipalName{
+			GroupName: &groupName,
+			UserName:  &username,
+		}
+
+		client := testAccProvider.Meta().(*Meta).Groups
+		ctx := testAccProvider.Meta().(*Meta).StopContext
+		resp, err := client.ListMembers(ctx, parentName)
+		if err != nil {
+			if resp.IsHTTPStatus(404) || !isPrincipalMemberOf(principalName, resp.Members) {
+				return nil
+			}
+			return err
+		}
+
+		return fmt.Errorf("Databricks group member still exists:\n%#v", resp)
+	}
+
+	return nil
+}
+
+func testAccDatabricksGroupMemberBasic(groupName string) string {
 	return fmt.Sprintf(`
 data "databricks_group_members" "test" {
   name = "admins"
